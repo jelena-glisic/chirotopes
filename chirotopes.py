bbae9e9284babb200bc19fe0845262fba300c98e
Jelena Glišić <57775129+jelena-glisic@users.noreply.github.com>
2024-04-16 14:54:11 +0200

    Update chirotopes.py

--- chirotopes.py
+++ chirotopes.py
@@ -47,7 +47,7 @@ def find_perm_value(P,c):
       else: return -1
     i+=1
 
-#function to check 3-term Grassmann–Plucker relations
+
 def check_3gp(c):
   '''
   idea: two for loops to pick the two elements b1, b2
